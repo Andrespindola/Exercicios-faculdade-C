d0c2753d23071ee181d87ac9ef96c07f0ebea10b
Andre Spindola <39387703+Andrespindola@users.noreply.github.com>
2018-10-17 11:54:09 -0300

    melhorando o codigo
    alguns adicionais do codigo.

--- vetores-exercicio-3.c
+++ vetores-exercicio-3.c
@@ -3,18 +3,25 @@
 
 int main() {
 
-  int quantVetor,vetPar,VetImpar,quantPar[100],quantImpar[100],posicao[100];
+  int quantVetor,vetPar[100],vetImpar[100],quantPar[100],quantImpar[100],posicao[100];
 
   printf("Digite o tamanho do vetor que deseja\n");
   scanf("%d",&quantVetor);
-
+  //
+  printf("Atribua os valores ao vetor\n");
   for (int i = 0; i < quantVetor ; i++) {
-    printf("Atribua os valores ao vetor\n");
     scanf("%d", &posicao[i]);
-  }
+    if(posicao[i] % 2 == 0){
+      vetPar[i]=posicao[i];
+    } else{
+      vetImpar[i]=posicao[i];
+    } // fim else
+  } // fim for
+  //
   printf("\n\n");
+  //
   for (int i = 0; i < quantVetor; i++) {
     printf("valor da posicao :  %d\n", posicao[i]);
-  }
+  } // fim for
   return 0;
 }
