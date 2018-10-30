f15502bef8d3321d6e4834e31e395138fe60889b
Andre Spindola <39387703+Andrespindola@users.noreply.github.com>
2018-10-30 17:39:56 -0300

    update codigo
    melhorias do codigo, adicionado o metodo bubble sort, para uma melhor visualização do resultado para o usuario.

--- vetores-exercicio-3.c
+++ vetores-exercicio-3.c
@@ -3,7 +3,7 @@
 
 int main() {
 
-  int quantVetor = 0,vetPar[100],vetImpar[100],quantPar = 0,quantImpar = 0,posicao[100],i;
+  int aux=0,quantVetor = 0,vetPar[100],vetImpar[100],quantPar = 0,quantImpar = 0,posicao[100],i;
 
   printf("Digite o tamanho do vetor que deseja\n");
   scanf("%d",&quantVetor);
@@ -22,13 +22,35 @@ int main() {
   } // fim for
   //
   printf("\n\n");
-  //
-  for (i = 0; i < quantPar; i++) {
-    printf("par %d\n\n",vetPar[i] );
-  } // fim for
-  for (int i = 0; i < quantImpar; i++) {
-    printf("impar %d\n\n",vetImpar[i] );
-  } // fim for
+  //bubble sort - metodo para deixar o vetor em ordem crescente - PARES
+  for (int indice = 1; indice < quantPar; indice++) {
+   for (i = 0; i < quantPar - 1; i++) {
+     if (vetPar[i] > vetPar[i + 1]) {
+       aux = vetPar[i];
+       vetPar[i] = vetPar[i + 1];
+       vetPar[i + 1] = aux;
+     }
+   }
+ }
+ //bubble sort - metodo para deixar o vetor em ordem crescente - IMPARES
+ for (int indice = 1; indice < quantPar; indice++) {
+  for (i = 0; i < quantPar - 1; i++) {
+    if (vetImpar[i] > vetImpar[i + 1]) {
+      aux = vetImpar[i];
+      vetImpar[i] = vetImpar[i + 1];
+      vetImpar[i + 1] = aux;
+    }
+  }
+}
+printf("Numeros pares : ");
+for (i = 0; i < quantPar; i++) {
+  printf(" [%d] ",vetPar[i]);
+} // fim for
+printf("\n");
+printf("Numero Impares : ");
+for (int i = 0; i < quantImpar; i++) {
+  printf(" [%d] ",vetImpar[i] );
+} // fim for
+
   return 0;
 }
-
