babf340a5e1d9c10697ceb97fd11a0080512b22c
Andre Spindola <39387703+Andrespindola@users.noreply.github.com>
2018-11-19 08:41:50 -0200

    melhorias no codigo

--- calculo-imposto.c
+++ calculo-imposto.c
@@ -6,34 +6,47 @@ Materia logica de programacao e algoritmos.
 Aula pratica - Exercicio 2(faz o calculo do imposto de renda).
 */
 //Inclusao das bibliotecas
+#include "pch.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-  //declaracao das variaveis
-  float salario = 0, salarioTotal = 0,desconto = 0;
-  //pede o salario
-  printf("Seja bem vindo a calculadora de imposto de renda.\n");
-  printf("-------------------------------------------------\n");
-  printf("Qual o seu salario mensal?.\n");
-  scanf("%f",&salario);
-  //faz as comparacoes dos valores, e mostra o seu respectivo valor na tela.
-  if (salario >= 4664.69){
-    salarioTotal = (((salario * 0.275) - 869.36) - salario) * -1;
-  }
-  else if ((salario >= 3751.06) && (salario <= 4664.68)) {
-    salarioTotal = (((salario * 0.225) - 636.13) - salario) * -1;
-  }
-  else if ((salario >= 2826.66) && (salario <= 3751.05)) {
-    salarioTotal = (((salario * 0.15) - 354.80) - salario) * -1;
-  }
-  else if ((salario >= 1903.99) && (salario <= 2825.65)) {
-    salarioTotal = (((salario * 0.075) - 142.80) - salario) * -1;
-  }
-  else { //teste
-    salarioTotal = salario;
-  }
-  printf("\nO seu salario era de R$%.2f, o valor do imposto foi de R$%.2f\n",salario,salario - salarioTotal);
-  printf("O valor total do seu salario ficou em R$%.2f\n", salarioTotal);
-  return 0;
+	//declaracao das variaveis
+	float salario = 0.0, salarioTotal = 0.0,desconto = 0.0;
+	//pede o salario
+	printf("Seja bem vindo a calculadora de imposto de renda.\n");
+	printf("-------------------------------------------------\n");
+	printf("Qual o seu salario mensal?.\n");
+	scanf_s("%f", &salario);
+	printf("\n");//apenas uma quebra linha para melhor visualizacao do resultado.
+	//faz as comparacoes dos valores, e mostra o seu respectivo valor na tela.
+	if (salario > 4664.68) {
+		desconto = (salario * 0.275) - 869.36;
+		salarioTotal = salario - desconto;
+	}
+	else if ((salario > 3751.05) && (salario < 4664.68)) {
+		desconto = ((salario * 0.225) - 636.13);
+		salarioTotal = salario - desconto;
+	}
+	else if ((salario > 2826.65) && (salario < 3751.05)) {
+		desconto = ((salario * 0.15) - 354.80);
+		salarioTotal = salario - desconto;
+	}
+	else if ((salario > 1903.98) && (salario < 2826.65)) {
+		desconto = ((salario * 0.075) - 142.80);
+		salarioTotal = salario - desconto;
+	}
+	else {
+		salarioTotal = salario;
+	}
+	//if para comparacao de desconto, como nao tem imposto de renda o valor continua o mesmo.
+	if (desconto == 0) {
+		printf("Devido ao seu salario ser inferior ha R$1903.98, voce nao tera desconto de imposto.\n");
+		printf("O seu salario continuara sendo R$%.2f.\n", salarioTotal);
+	}
+	else { //mostra ao usuario caso o seu salario tenha desconto.
+		printf("\nO seu salario era de R$%.2f, o valor do imposto foi de R$%.2f\n", salario, desconto);
+		printf("O valor total do seu salario ficou em R$%.2f\n", salarioTotal);
+	}
+	return 0;
 }
